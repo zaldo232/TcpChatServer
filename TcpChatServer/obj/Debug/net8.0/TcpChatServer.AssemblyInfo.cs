c7fa591970535f10057ae89afe75299e16b66884
zaldo232 <zaldo232@naver.com>
2025-05-12 04:28:38 +0900

    드래그로 파일 올리기

--- TcpChatServer/obj/Debug/net8.0/TcpChatServer.AssemblyInfo.cs
+++ TcpChatServer/obj/Debug/net8.0/TcpChatServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926ce812f432c452b0227416931714b740557cb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b72bf72a6b05d028aa787982880dbad3083a29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
