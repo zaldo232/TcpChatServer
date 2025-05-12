210dcae2b4c306aeff33c5e2ad641d0042372ca9
zaldo232 <zaldo232@naver.com>
2025-05-12 21:26:16 +0900

    실시간 읽음 추가(리스트에)

--- TcpChatServer/obj/Debug/net8.0/TcpChatServer.AssemblyInfo.cs
+++ TcpChatServer/obj/Debug/net8.0/TcpChatServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b72bf72a6b05d028aa787982880dbad3083a29c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec493e6ead80b1f349d052c12b18b786b2ae91b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TcpChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
